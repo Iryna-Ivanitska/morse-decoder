c133f18b6475a6d1cfc9fdca6526a0a843c24149
Iryna Ivanytska <ira.pechenitsya@gmail.com>
2021-08-18 10:52:48 +0300

    feat: add Morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let output = '';
+    
+    for (let i = 0; i < expr.length / 10; i++) {
+        let zeros = expr.slice(i*10,i*10+10);
+        if (zeros === '**********') {
+            output += ' ';
+    } else {
+        let number = parseInt(zeros).toString();
+        let code = '';
+
+        for (let j = 0; j < number.length / 2; j++) {
+            if (number.slice(j*2,j*2+2) == '10') code += '.';
+            if (number.slice(j*2,j*2+2) == '11') code += '-';
+        }
+        output += MORSE_TABLE[code]; 
+    } 
+          
+}
+return output;
 }
 
 module.exports = {
